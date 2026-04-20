3a19e2762d5d27b12eeab1da77223a79a0da5260
Maxima Freiman <maximafreiman@gmail.com>
2026-04-20 14:57:35 +0700

    Create 08. HASH160 + Checksum Result.py

---
+++ 01. Legacy/08. HASH160 + Checksum Result.py
@@ -0,0 +1,8 @@
+hash160 = "4f6e669e65be170eae07d9a7d81e181a9886fec3"
+checksum = "9ba94fc0"
+
+final_hex = hash160 + checksum
+
+print("HASH160     :", hash160)
+print("Checksum    :", checksum)
+print("Final Hex   :", final_hex)
