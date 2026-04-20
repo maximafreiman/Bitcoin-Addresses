e0d21b448e825452d236363c7555cccc90d1bfa4
Maxima Freiman <maximafreiman@gmail.com>
2026-04-20 15:03:55 +0700

    Create 09. Mainnet Legacy Version Byte + Hash160 + Checksum.py

---
+++ 01. Legacy/09. Mainnet Legacy Version Byte + Hash160 + Checksum.py
@@ -0,0 +1,15 @@
+# Tambahkan version byte 00 + HASH160 + checksum
+# tanpa library sama sekali
+
+hash160  = "4f6e669e65be170eae07d9a7d81e181a9886fec3"
+checksum = "9ba94fc0"
+
+version_byte = "00"
+
+# gabungkan semuanya
+final_hex = version_byte + hash160 + checksum
+
+print("Version Byte :", version_byte)
+print("HASH160      :", hash160)
+print("Checksum     :", checksum)
+print("Final Hex    :", final_hex)
