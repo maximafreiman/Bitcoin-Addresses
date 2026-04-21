5bb3e992d26f87d43d0b3197bc15abdc04d8b3df
Maxima Freiman <maximafreiman@gmail.com>
2026-04-21 19:58:03 +0700

    Update 10. Base58 Encoding (And Get your Legacy Address).py

--- 01. Legacy/10. Base58 Encoding (And Get your Legacy Address).py
+++ 01. Legacy/10. Base58 Encoding (And Get your Legacy Address).py
@@ -1,4 +1,4 @@
-data_hex = "004f6e669e65be170eae07d9a7d81e181a9886fec39ba94fc0"
+data_hex = "004f6e669e65be170eae07d9a7d81e181a9886fec39ba94fc0"    # <----------- masukkan hasil dari version + checksum + hash160
 
 alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
@@ -12,12 +12,10 @@ while num > 0:
     num, rem = divmod(num, 58)
     encoded = alphabet[rem] + encoded
 
-# cek leading zero byte ("00")
-# setiap 00 di depan = tambah "1"
 leading_zero_bytes = 0
 
-for i in range(0, len(data_hex), 2):
-    if data_hex[i:i+2] == "00":
+for i in range(0, len(data_hex), 2):                 # cek leading zero byte ("00")
+    if data_hex[i:i+2] == "00":                      # setiap 00 di depan = tambah "1"
         leading_zero_bytes += 1
     else:
         break
