47c20b61de87d258c245d1a9ad2569cb719922bc
Maxima Freiman <maximafreiman@gmail.com>
2026-04-21 19:59:48 +0700

    Update 08. HASH160 + Checksum Result.py

--- 01. Legacy/08. HASH160 + Checksum Result.py
+++ 01. Legacy/08. HASH160 + Checksum Result.py
@@ -1,7 +1,7 @@
 hash160 = "4f6e669e65be170eae07d9a7d81e181a9886fec3"  # <------------ masukkan hash160 dari ripemd160
 checksum = "9ba94fc0" # <------------ masukkan checksum dari hasil double_sha256
 
-final_hex = hash160 + checksum  # menggabungkan hash160 + 
+final_hex = hash160 + checksum  # menggabungkan hash160 + checksum
 
 print("HASH160     :", hash160)
 print("Checksum    :", checksum)
